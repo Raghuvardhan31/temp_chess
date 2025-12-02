3faeaae74d15ef63305694ede49ea9e14dc00f7d
Raghuvardhan <bonjuriraghuvardhan@gmail.com>
2025-12-02 16:02:25 +0530

    included domain2

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/temp_chess/", // <--- important for GitHub Pages
+  base: "/", // <--- correct for custom domain
 });
